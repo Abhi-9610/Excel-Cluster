8d410cfbecca15af19163e90f29fa7db78f8961d
Abhishek <av2935288@gmail.com>
2024-11-27 17:43:09 +0530

    new

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import firebase_admin
 from firebase_admin import credentials, storage
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__)
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('serviceAccountKey.json')
